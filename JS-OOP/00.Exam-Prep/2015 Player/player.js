ff4cb56e870109498055d5928d4d54465ff23fb9
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-02-03 10:23:37 +0200

    final update of player.js

--- JS-OOP/00.Exam-Prep/2015 Player/player.js
+++ JS-OOP/00.Exam-Prep/2015 Player/player.js
@@ -8,6 +8,34 @@ function solve() {
                 IMDB_MAX_RATING: 5
             };
 
+        function indexOfPlayableFromCollecttion(collection, id) {
+            var i, len;
+            for (i = 0, len = collection.length; i < len; i += 1) {
+                if (collection.id === id) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        function getSortingFunction(firstParameter, secondParameter) {
+            return function(first, second) {
+                if (first[firstParameter] < second[firstParameter]) {
+                    return -1;
+                } else if (first[firstParameter] > second[firstParameter]) {
+                    return 1;
+                }
+
+                if (first[secondParameter] < second[secondParameter]) {
+                    return -1;
+                } else if (first[secondParameter] > second[secondParameter]) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        }
+
         validator = {
             validateIfUndefined: function(val, name) {
                 name = name || 'value';
@@ -53,19 +81,19 @@ function solve() {
                     throw new Error('Imdb rating must be between ' + CONSTANTS.IMDB_MIN_RATING + ' and ' + CONSTANTS.IMDB_MAX_RATING + '!');
                 }
             },
-            validatePlayableId: function(id) {
-                this.validateIfUndefined(id, 'Playable id');
+            validateId: function(id) {
+                this.validateIfUndefined(id, 'Object id');
                 if (typeof(id) !== 'number') {
                     id = id.id;
                 }
 
-                this.validateIfUndefined(id, 'Playable must have id!');
+                this.validateIfUndefined(id, 'Object must have id!');
                 return id;
             },
             validatePageSize: function(page, size, maxElements) {
                 this.validateIfUndefined(page);
                 this.validateIfNumber(page);
-                this, validateIfUndefined(size);
+                this.validateIfUndefined(size);
                 this.validateIfNumber(size);
                 if (page < 0) {
                     throw new Error('Page must be greater than 0!');
@@ -77,7 +105,7 @@ function solve() {
                 }
             }
         };
-        
+
         player = (function() {
             var currentPlayerId = 0,
                 player = Object.create({});
@@ -109,35 +137,131 @@ function solve() {
             return player;
         }());
 
+        player = (function() {
+            var currentPlayerId = 0,
+                player = Object.create({});
+
+            Object.defineProperty(player, 'init', {
+                value: function(name) {
+                    this.name = name;
+                    this._id = ++currentPlayerId;
+                    this._playlists = [];
+                    return this;
+                }
+            });
+
+            Object.defineProperty(player, 'id', {
+                get: function() {
+                    return this._id;
+                }
+            })
+
+            Object.defineProperty(player, 'name', {
+                get: function() {
+                    return this._name;
+                },
+                set: function(val) {
+                    validator.validateString(val, 'Player name');
+                    this._name = val;
+                }
+            });
+
+            Object.defineProperty(player, 'addPlaylist', {
+                value: function(playlist) {
+                    validator.validateIfUndefined(playlist, 'Playlist does not exist!');
+                    if (playlist.id === undefined) {
+                        throw new Error('Playlist must have ID!');
+                    }
+                    this._playlists.push(playlist);
+                    return this;
+                }
+            });
+
+            Object.defineProperty(player, 'getPlaylistById', {
+                value: function(id) {
+                    var i, len;
+                    validator.validateIfUndefined(id, 'Playlist id');
+                    validator.validateIfNumber(id, 'Playlist id');
+                    var foundIndex = indexOfPlayableFromCollecttion(this._playlists, id);
+                    if (foundIndex < 0) {
+                        return null;
+                    }
+                    return this._playlists[foundIndex];
+                }
+            });
+
+            Object.defineProperty(player, 'contains', {
+                value: function(playable, playlist) {
+                    validator.validateIfUndefined(playable);
+                    validator.validateIfUndefined(playlist);
+                    var playableId = validator.validateId(playable.id);
+                    var playlistId = validator.validateId(playlist.id);
+
+                    var playlist = this.getPlaylistById(playlistId);
+                    if (foundplaylist === null) {
+                        return false;
+                    }
+                    var playable = playlist.getPlayableById(playableId);
+                    if (playable === null) {
+                        return false;
+                    }
+                    return true;
+                }
+            });
+
+            Object.defineProperty(player, 'search', {
+                value: function(pattern) {
+                    validator.validateString(pattern, 'Pattern');
+                    return this._playlists
+                        .filter(function(playlist) {
+                            return playlist.listPlayables()
+                                .some(function(playable) {
+                                    return playable.length !== undefined &&
+                                        playable
+                                        .title
+                                        .toLowerCase()
+                                        .indexOf(pattern.toLowerCase()) >= 0
+                                });
+                        })
+                        .map(function(playlist) {
+                            return {
+                                id: playlist.id,
+                                name: playlist.name
+                            };
+                        });
+                }
+            });
+
+            return player;
+        }());
+
+        Object.defineProperty(player, 'removePlaylist', {
+            valie: function(id) {
+                id = validator.validateId(id);
+
+                var foundIndex = indexOfPlayableFromCollecttion(this._playlists, id);
+
+                if (foundIndex < 0) {
+                    throw new Error('Playlist with id ' + id + ' was not found!');
+                }
+                this._playlists.splise(foundIndex, 1);
+                return this;
+            }
+        });
+
+        Object.defineProperty(player, 'listPlaylists', {
+            value: function(page, size) {
+                validator.validatePageSize(page, size, this._playlists.length);
+                return this._playlists
+                    .slice()
+                    .sort(getSortingFunction('name', 'id'))
+                    .splice(page * size, size);
+            }
+        });
+
         playlist = (function() {
             var currentPlaylistId = 0,
                 playlist = Object.create({});
-
-            function indexOfPlayableFromPlaylist(id) {
-                var i, len;
-                for (i = 0, len = this._playables.length; i < len; i += 1) {
-                    if (this._playables.id === id) {
-                        return i;
-                    }
-                }
-                return -1;
-            }
-
-            function sortByTitleAndId(first, second) {
-                if (first.title < second.title) {
-                    return -1;
-                } else if (first.title > second.title) {
-                    return 1;
-                }
-
-                if (first.id < second.id) {
-                    return -1;
-                } else if (forst.id > second.id) {
-                    return 1;
-                } else {
-                    return 0;
-                }
-            }
 
             Object.defineProperty(playlist, 'init', {
                 value: function(name) {
@@ -156,7 +280,7 @@ function solve() {
                     validator.validateString(val, 'Playlist name');
                     this._name = val;
                 }
-            })
+            });
 
             Object.defineProperty(playlist, 'addPlayable', {
                 value: function(playable) {
@@ -174,7 +298,7 @@ function solve() {
                     var i, len;
                     validator.validateIfUndefined(id, 'Playlist id');
                     validator.validateIfNumber(id, 'Playlist id');
-                    var foundIndex = indexOfPlayableFromPlaylist.call(this, id);
+                    var foundIndex = indexOfPlayableFromCollecttion(this._playables, id);
                     if (foundIndex < 0) {
                         return null;
                     }
@@ -184,9 +308,9 @@ function solve() {
 
             Object.defineProperty(playlist, 'removePlayable', {
                 valie: function(id) {
-                    id = validator.validatePlayableId(id);
+                    id = validator.validateId(id);
 
-                    var foundIndex = indexOfPlayableFromPlaylist.call(this, id);
+                    var foundIndex = indexOfPlayableFromCollecttion(this._playables, id);
 
                     if (foundIndex < 0) {
                         throw new Error('Playable with id ' + id + ' was not found!');
@@ -198,11 +322,13 @@ function solve() {
 
             Object.defineProperty(playlist, 'listPlayables', {
                 value: function(page, size) {
+                    page = page || 0;
+                    size = size || Number.MAX_SAFE_INTEGER;
                     validator.validatePageSize(page, size, this._playables.length);
                     return this
                         ._playables
                         .slice()
-                        .sort(sortByTitleAndId)
+                        .sort(getSortingFunction('title', 'id'))
                         .splice(page * size, size);
                 }
             });
@@ -325,6 +451,7 @@ function solve() {
 
         return {
             getPlayer: function(name) {
+                return Object.create(player).init(name);
                 // returns new player instance with provided name
             },
             getPlaylist: function(name) {
@@ -351,17 +478,24 @@ function solve() {
 var module = solve();
 
 var playlist = module.getPlaylist('My playlist');
+var player = module.getPlayer('pesho');
 
-for (var i = 1; i < 10; i++) {
+for (var i = 1; i < 3; i++) {
     var currentAudio = module.getAudio("Audio " + i, "Author " + i, i * 0.75);
     console.log(currentAudio.play());
     playlist.addPlayable(currentAudio);
 }
 
-for (var i = 1; i < 10; i++) {
+for (var i = 1; i < 3; i++) {
     var currentVideo = module.getVideo("Video " + i, "Author " + i, 3.5);
     console.log(currentVideo.play());
     playlist.addPlayable(currentVideo);
 }
+var audio = module.getAudio('ivan', 'ivanov', 4);
+playlist.addPlayable(audio);
 
 console.log(playlist);
+
+console.log('------------------')
+player.addPlaylist(playlist);
+console.log(player.search('van'));
