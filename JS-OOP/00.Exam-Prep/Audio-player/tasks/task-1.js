e1fc1c75b5f60d6ce016f55b02ed1ea9a8af68ba
zachdimitrov <zahari_d@abv.bg>
2017-02-21 23:13:07 +0200

    audio player tas  complete with es6 classes

--- JS-OOP/00.Exam-Prep/Audio-player/tasks/task-1.js
+++ JS-OOP/00.Exam-Prep/Audio-player/tasks/task-1.js
@@ -110,9 +110,11 @@ function solve() {
         }
     }
 
-    class Player {
+    class PlayList {
         constructor(name) {
             this.name = name;
+            this.id = getId();
+            this.list = [];
         }
 
         get name() {
@@ -120,19 +122,122 @@ function solve() {
         }
 
         set name(val) {
-            validate.ifStringIsCorrect();
-            shis._name = val;
+            validate.ifStringIsCorrect(val);
+            this._name = val;
         }
 
+        addPlayable(playable) {
+            if (playable.id >= 0) {
+                this.list.push(playable);
+            } else {
+                throw new Error("Item must be playable!");
+            }
+            return this;
+        }
 
+        getPlayableById(id) {
+            validate.ifNumberIsCorrect(id, 'Id');
+            return this.list.find(x => x.id === id) || null;
+        }
+
+        removePlayable(playable) {
+            let i = -1;
+            if (typeof(playable) === 'number') {
+                i = this.list.indexOf(this.list.find(x => x.id === playable));
+            } else if (playable.id >= 0) {
+                i = this.list.indexOf(playable);
+            } else {
+                throw new Error("Item must be playable!");
+            }
+            if (i >= 0) {
+                this.list.splice(i, 1);
+            } else {
+                throw new Error("This item is not in the list!");
+            }
+            return this;
+        }
+
+        listPlayables(page, size) {
+            if (typeof(page) !== 'number' || typeof(size) !== 'number' || page < 0 || size < 0 || page * size >= this.list.length) {
+                throw new Error('Page * size must be less than items count');
+            }
+            let result = [];
+            for (var i = page * size; i < page * size + size; i++) {
+                if (this.list[i]) {
+                    result.push(this.list[i]);
+                }
+            }
+            return result;
+        }
+    }
+
+    class Player {
+        constructor(name) {
+            this.name = name;
+            this.id = getId();
+            this.playLists = [];
+        }
+
+        get name() {
+            return this._name;
+        }
+
+        set name(val) {
+            validate.ifStringIsCorrect(val);
+            this._name = val;
+        }
+
+        addPlaylist(playlistToAdd) {
+            if (playlistToAdd instanceof PlayList) {
+                this.playLists.push(playlistToAdd);
+            } else {
+                throw new Error('Item to add must be Playlist');
+            }
+            return this;
+        }
+
+        getPlaylistById(id) {
+            validate.ifNumberIsCorrect(id, 'Id');
+            return this.playLists.find(x => x.id === id) || null;
+        }
+
+        removePlaylist(p) {
+            let i = -1;
+            if (typeof(p) === 'number') {
+                i = this.playLists.indexOf(this.playLists.find(x => x.id === p));
+            } else if (p.id >= 0) {
+                i = this.playLists.indexOf(p);
+            } else {
+                throw new Error("Item must be Playlist!");
+            }
+            if (i >= 0) {
+                this.playLists.splice(i, 1);
+            } else {
+                throw new Error("This item is not in the list!");
+            }
+            return this;
+        }
+
+        listPlaylists(page, size) {
+            if (typeof(page) !== 'number' || typeof(size) !== 'number' || page < 0 || size < 0 || page * size >= this.list.length) {
+                throw new Error('Page * size must be less than items count');
+            }
+            let result = [];
+            for (var i = page * size; i < page * size + size; i++) {
+                if (this.playLists[i]) {
+                    result.push(this.playLists[i]);
+                }
+            }
+            return result;
+        }
     }
 
     var module = {
         getPlayer: function(name) {
-            // returns a new player instance with the provided name
+            return new Player(name);
         },
         getPlaylist: function(name) {
-            //returns a new playlist instance with the provided name
+            return new PlayList(name);
         },
         getAudio: function(title, author, length) {
             return new Audio(title, author, length);
