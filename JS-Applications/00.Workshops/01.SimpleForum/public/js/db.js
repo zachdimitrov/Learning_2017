bc0e423a12845a5163ddc13b8da57a50c8459cd5
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-04-11 12:52:44 +0300

    crate a simple app

---
+++ JS-Applications/00.Workshops/01.SimpleForum/public/js/db.js
@@ -0,0 +1,62 @@
+var db = function() {
+    var items = [{ name: "John", id: 1000 }, { name: "Pesho", id: 1001 }, { name: "Jane", id: 42 }];
+
+    function getAjax() {
+        var promise = new Promise((resolve, reject) => {
+            $.ajax({
+                url: "api/items",
+                method: "GET",
+                contentType: "application/json",
+                success: function(res) {
+                    resolve(res);
+                },
+                error: function(err) {
+                    reject(err);
+                }
+            })
+        });
+
+        return promise;
+    }
+
+    function get() {
+        var promise = new Promise((resolve, reject) => {
+            resolve({ result: items, length: items.length });
+        });
+
+        return promise;
+    }
+
+    function getById(id) {
+        id = +id;
+        var promise = new Promise(function(resolve, reject) {
+            for (var i = 0; i < items.length; i += 1) {
+                if (items[i].id === id) {
+                    resolve({ result: items[i] });
+                    return;
+                }
+            }
+            reject({ msg: "Invalid id!" });
+        });
+
+        return promise;
+    }
+
+    var lastId = 0;
+
+    function save(item) {
+        var promise = new Promise((resolve, reject) => {
+            item.id = lastId += 1;
+            items.push(item);
+            resolve("Success!");
+        });
+
+        return promise;
+    }
+
+    return {
+        get,
+        getById,
+        save
+    }
+}();
