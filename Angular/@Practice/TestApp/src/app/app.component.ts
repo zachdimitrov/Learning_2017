a080dd0ccbd25875d32e9d2423cc467c1b876300
Zahari <zach_dimitrov@yahoo.com>
2017-08-16 09:23:15 +0300

    Add Hero title

--- Angular/@Practice/TestApp/src/app/app.component.ts
+++ Angular/@Practice/TestApp/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`,
+  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
